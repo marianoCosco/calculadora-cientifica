b7b19e0cf1c55dce6496d9f3545374203ba42c2f
mariano cosco <mariancosco1406@gmail.com>
2025-09-16 17:58:00 -0300

    se crearon los componentes y se hicieron los primeros estilos

---
+++ calculadora-cientifica/src/components/DisplayWindow.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayWindow = () => {
+  return <div>DisplayWindow</div>;
+};
+
+export default DisplayWindow;
---
+++ calculadora-cientifica/src/components/KeysWindow.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const KeysWindow = () => {
+  return <div>KeysWindow</div>;
+};
+
+export default KeysWindow;
